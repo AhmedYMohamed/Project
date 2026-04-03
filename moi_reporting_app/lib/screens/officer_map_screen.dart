0ec5341c9560b44c4ecc043a4587bb015e1f87a6
AhmedYMohamed <ahmedya618@gmail.com>
2026-04-03 11:41:20 +0200

    Latlong fix in officers app screen:

--- moi_reporting_app/lib/screens/officer_map_screen.dart
+++ moi_reporting_app/lib/screens/officer_map_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong2.dart';
+import 'package:latlong2/latlong.dart';
 import 'officer_report_details_screen.dart';
 
 class OfficerMapScreen extends StatelessWidget {
